95a2589f6fc24bf6e0ee5b2535d0dceaf0bc52a5
Angel <Angel>
2022-06-09 02:47:26 -0600

    nuevo_commit

--- hola mundo 3/main.cpp
+++ hola mundo 3/main.cpp
@@ -6,9 +6,9 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    cout << "hola mundo xd"<<endl;
     return 0;
 }
